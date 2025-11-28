60c70231b6d7e9b10666bccb52f106b27745515b
hiroakiwuteng-art <hiroakiwuteng@gmail.com>
2025-11-28 17:51:23 +0900

    Revert "スライム"
    This reverts commit ab9c260f9d8e1773d2463e857b4b0f2642f4541c, reversing
    changes made to cf3d3d15162cadee06d208635aa7520ff9f1a23f.

--- Assets/0_Critical/Hiroaki/スクリプト/mono/pure/Slime/SlimeController.cs
+++ Assets/0_Critical/Hiroaki/スクリプト/mono/pure/Slime/SlimeController.cs
@@ -4,6 +4,4 @@ public class SlimeController:MonoBehaviour
 {
     public Animator animator;
     public Transform target;
-    public GameObject normalCore;
-    public GameObject jumpCore;
 }
--- Assets/0_Critical/Hiroaki/スクリプト/mono/pure/Slime/SlimeRefs.cs
+++ Assets/0_Critical/Hiroaki/スクリプト/mono/pure/Slime/SlimeRefs.cs
@@ -4,6 +4,4 @@ public class SlimeRefs
 {
     public Transform target;
     public Animator animator;
-    public GameObject normalCore;
-    public GameObject jumpCore;
 }
