c01a6d922c9885ec29077e42712d175e4886d5a9
nekozuru <sola24neko@icloud.com>
2025-12-02 19:42:46 +0900

    SwitchWeapon

--- Assets/0_Critical/ねこゾロア/scripts/UI.cs
+++ Assets/0_Critical/ねこゾロア/scripts/UI.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class ShowUIWhileTabPressed : MonoBehaviour
 {
+    public bool Openweapon;
     public GameObject uiPanel;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.Tab))
         {
+            Openweapon = true;
             uiPanel.SetActive(true);
         }
         else
         {
+            Openweapon = false;
             uiPanel.SetActive(false);
         }
     }
